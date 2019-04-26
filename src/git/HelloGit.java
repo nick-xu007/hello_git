8aeacc8dc94f427d62369664e2db091b520de41b
nick_xu <nick_xu007@163.com>
2019-04-26 14:37:50 +0800

    测试Git

---
+++ src/git/HelloGit.java
@@ -0,0 +1,12 @@
+package git;
+
+/**
+ * @Desc 测试git的使用
+ * @Author Nick_Xu
+ * @Date 2019/4/26 14:36
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("git的使用...");
+    }
+}
